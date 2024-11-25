a643de917384a31917fbdf8dc35125272071c7dc
AdamKuraczynski <kuraczynski.adam@gmail.com>
2024-11-25 19:50:26 +0100

    Changes - agp version update - android studio updated to new version - BlockedActivity manifest updated to only allow single task and noHistory - BlockScreen on back button will go to home screen - onPause and onStop now ends the BlockedActivity

--- app/src/main/java/com/adamkuraczynski/focusfortress/blocking/BlockedActivity.kt
+++ app/src/main/java/com/adamkuraczynski/focusfortress/blocking/BlockedActivity.kt
@@ -3,13 +3,23 @@ package com.adamkuraczynski.focusfortress.blocking
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import com.adamkuraczynski.focusfortress.ui.theme.FocusFortressTheme
 
 class BlockedActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                goToHomeScreen()
+            }
+        })
+
         val blockType = intent.getStringExtra("BLOCK_TYPE") ?: "app"
+
         setContent {
             FocusFortressTheme {
                 BlockedScreen(
@@ -20,6 +30,17 @@ class BlockedActivity : ComponentActivity() {
                 )
             }
         }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
     }
 
     private fun goToHomeScreen() {
